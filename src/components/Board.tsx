802befadbd1fde4060e75452316ce0365f8e6bf1
k2works <kakimomokuri@gmail.com>
2018-05-28 18:44:10 +0900

    React Tutorial
    Showing the Moves

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,33 +1,15 @@
 import * as React from "react";
 import Square from "./Square";
-import { SquareType} from "./Square";
 
-interface IBoardState {
-    squares:SquareType[];
-    xIsNext:boolean;
+interface IBoardProps {
+    squares: Array<('O' | 'X' | null)>;
+    onClick: (i: number) => void;
 }
 
-class Board extends React.Component<any, IBoardState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
+class Board extends React.Component<IBoardProps, any> {
     public render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -49,44 +31,14 @@ class Board extends React.Component<any, IBoardState> {
 
     private renderSquare(i: number) {
         return <Square
-            value={this.state.squares[i]}
-            onClick={this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={this.onClick(i)}
         />;
     }
 
-    private handleClick(i: number) {
-        return () => {
-            const squares = this.state.squares.slice();
-            if (calculateWinner(squares) || squares[i]) {
-                return;
-            }
-            squares[i] = this.state.xIsNext ? 'X' : 'O';
-            this.setState({
-                squares,
-                xIsNext: !this.state.xIsNext,
-            });
-        }
+    private onClick(i: number) {
+        return () => this.props.onClick(i);
     }
 }
 
 export default Board;
-
-function calculateWinner(squares: SquareType[]) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 6],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (const line of lines) {
-        const [a, b, c] = line;
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
