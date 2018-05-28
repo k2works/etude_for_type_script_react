715b6ed283db8d494db7fc1499f7172db38b6ce9
k2works <kakimomokuri@gmail.com>
2018-05-28 14:25:36 +0900

    React Tutorial
    Getting Started

---
+++ src/components/Square.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Square extends React.Component {
+    public render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+export default Square;
