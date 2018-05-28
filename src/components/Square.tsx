53185f6cf3770dc5b027c1fbda4d899d2545acba
k2works <kakimomokuri@gmail.com>
2018-05-28 15:09:35 +0900

    React Tutorial
    An Interactive Component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,16 +1,31 @@
 import * as React from "react";
 
-export interface IProps {
+interface ISquareProps {
     value: number;
 }
 
-class Square extends React.Component<IProps, object> {
+interface ISquareState {
+    value: string | null
+}
+
+class Square extends React.Component<ISquareProps, ISquareState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     public render() {
         return (
-            <button className="square">
+            <button className="square" onClick={this.clickEvent()}>
                 {this.props.value}
             </button>
         );
+    }
+
+    private clickEvent() {
+        return () => this.setState({value: 'X'});
     }
 }
 
