c5753bdfa5cca49d0a610d93334c917a189ddd07
k2works <kakimomokuri@gmail.com>
2018-05-28 17:00:16 +0900

    React Tutorial
    Declaring a Winner

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -17,7 +17,13 @@ class Board extends React.Component<any, IBoardState> {
     }
 
     public render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -51,7 +57,10 @@ class Board extends React.Component<any, IBoardState> {
     private handleClick(i: number) {
         return () => {
             const squares = this.state.squares.slice();
-            squares[i] = 'X';
+            if (calculateWinner(squares) || squares[i]) {
+                return;
+            }
+            squares[i] = this.state.xIsNext ? 'X' : 'O';
             this.setState({
                 squares,
                 xIsNext: !this.state.xIsNext,
@@ -61,3 +70,23 @@ class Board extends React.Component<any, IBoardState> {
 }
 
 export default Board;
+
+function calculateWinner(squares: SquareType[]) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 6],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (const line of lines) {
+        const [a, b, c] = line;
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
