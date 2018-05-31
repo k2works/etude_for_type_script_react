396880a7f3f97accd5bf7e7f376289dd4dfbb7c9
k2works <kakimomokuri@gmail.com>
2018-05-31 16:09:42 +0900

    Redux Tutorial
    Store

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { EnthusiasmAction } from "./actions";
 import './App.css';
 import {BeerListContainer} from "./components/beerlist/BeerListContainer";
 import Game from "./components/game/Game";
 import Hello from "./containers/Hello";
+import { enthusiasm } from './reducers';
+import todoApp from './reducers/todo';
+import { IStoreState } from './types';
+
+const todoStore = createStore(todoApp);
+const helloStore = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
 
 const App = () => {
     return (
         <>
-            <Hello/>
+            <Provider store={helloStore}>
+              <Hello/>
+            </Provider>,
             <Game/>
             <BeerListContainer/>
         </>
